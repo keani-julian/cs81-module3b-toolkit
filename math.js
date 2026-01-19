00a840d73c8e5e35038ccb33298922c495513caf
keani-julian <julian_keani01@student.smc.edu>
2026-01-18 19:08:26 -0800

    Add square() function

--- math.js
+++ math.js
@@ -2,3 +2,36 @@
 function double(n) {
     return n * 2;
 }
+console.log (double(4));
+console.log (double(5));
+console.log (double(3));
+console.log (double(12));
+
+// square a number
+function square(n) {
+  return n * n;
+}
+// check if a number is even
+function isEven(n) {
+  return n % 2 === 0;
+}
+// check if a number is odd
+function isOdd(n) {
+  return n % 2 !== 0;
+}
+// multiply two numbers
+function multiply(a, b) {
+  return a * b;
+}
+// return the larger of two numbers
+function maxOfTwo(a, b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+console.log (square(4))
+console.log (isEven(0))
+console.log (isOdd(5))
+console.log (multiply(4,4))
+console.log (maxOfTwo(3,6))
