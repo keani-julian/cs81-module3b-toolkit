a4b5438492c15487b31fbbd122dbb2ac5a290af1
keani-julian <julian_keani01@student.smc.edu>
2026-01-18 19:37:40 -0800

    Add math.js from Math Toolkit

--- math.js
+++ math.js
@@ -30,8 +30,33 @@ function maxOfTwo(a, b) {
   }
   return b;
 }
+//1:5 
 console.log (square(4))
 console.log (isEven(0))
 console.log (isOdd(5))
 console.log (multiply(4,4))
 console.log (maxOfTwo(3,6))
+// 2:5
+console.log (square(16))
+console.log (isEven(8))
+console.log (isOdd(9))
+console.log (multiply(3,2))
+console.log (maxOfTwo(7,0))
+// 3:5
+console.log (square(2))
+console.log (isEven(1))
+console.log (isOdd(17))
+console.log (multiply(8,7))
+console.log (maxOfTwo(34,9))
+// 4:5
+console.log (square(100))
+console.log (isEven(6))
+console.log (isOdd(60))
+console.log (multiply(8,2))
+console.log (maxOfTwo(44,222))
+// 5:5
+console.log (square(25))
+console.log (isEven(75))
+console.log (isOdd(21))
+console.log (multiply(4,6))
+console.log (maxOfTwo(55,5))
